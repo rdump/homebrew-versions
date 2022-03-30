79c309d97d0e4d9d0919d31b24d890a91c7562d3
Richard Johnson <uthacalthing@gmail.com>
2022-03-30 09:02:02 -0600

    Update class version for kubernetes-cli 1.20.4

--- Formula/kubernetes-cli@1.20.4.rb
+++ Formula/kubernetes-cli@1.20.4.rb
@@ -1,4 +1,4 @@
-class KubernetesCliAT1194 < Formula
+class KubernetesCliAT1204 < Formula
   desc "Kubernetes command-line interface"
   homepage "https://kubernetes.io/"
   url "https://github.com/kubernetes/kubernetes.git",
