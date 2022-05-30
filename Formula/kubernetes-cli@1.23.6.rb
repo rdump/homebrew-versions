e06adbb61228f489318f9ff8c7ea0521c7f97338
Richard Johnson <uthacalthing@gmail.com>
2022-05-30 15:06:30 -0600

    kubernetes-cli 1.23.6 formula

---
+++ Formula/kubernetes-cli@1.23.6.rb
@@ -0,0 +1,63 @@
+class KubernetesCliAT1236 < Formula
+  desc "Kubernetes command-line interface"
+  homepage "https://kubernetes.io/"
+  url "https://github.com/kubernetes/kubernetes.git",
+      tag:      "v1.23.6",
+      revision: "ad3338546da947756e8a88aa6822e9c11e7eac22"
+  license "Apache-2.0"
+  head "https://github.com/kubernetes/kubernetes.git", branch: "master"
+
+  livecheck do
+    url :stable
+    regex(/^v?(1\.22(?:\.\d+)+)$/i)
+  end
+
+  depends_on "bash" => :build
+  depends_on "coreutils" => :build
+  depends_on "go" => :build
+
+  uses_from_macos "rsync" => :build
+
+  def install
+    # Don't dirty the git tree
+    rm_rf ".brew_home"
+
+    # Make binary
+    # Deparallelize to avoid race conditions in creating symlinks, creating an error like:
+    #   ln: failed to create symbolic link: File exists
+    # See https://github.com/kubernetes/kubernetes/issues/106165
+    ENV.deparallelize
+    ENV.prepend_path "PATH", Formula["coreutils"].libexec/"gnubin" # needs GNU date
+    system "make", "WHAT=cmd/kubectl"
+    bin.install "_output/bin/kubectl"
+
+    # Install bash completion
+    output = Utils.safe_popen_read(bin/"kubectl", "completion", "bash")
+    (bash_completion/"kubectl").write output
+
+    # Install zsh completion
+    output = Utils.safe_popen_read(bin/"kubectl", "completion", "zsh")
+    (zsh_completion/"_kubectl").write output
+
+    # Install fish completion
+    output = Utils.safe_popen_read(bin/"kubectl", "completion", "fish")
+    (fish_completion/"kubectl.fish").write output
+
+    # Install man pages
+    # Leave this step for the end as this dirties the git tree
+    system "hack/update-generated-docs.sh"
+    man1.install Dir["docs/man/man1/*.1"]
+  end
+
+  test do
+    run_output = shell_output("#{bin}/kubectl 2>&1")
+    assert_match "kubectl controls the Kubernetes cluster manager.", run_output
+
+    version_output = shell_output("#{bin}/kubectl version --client 2>&1")
+    assert_match "GitTreeState:\"clean\"", version_output
+    if build.stable?
+      revision = stable.specs[:revision]
+      assert_match revision.to_s, version_output
+    end
+  end
+end
