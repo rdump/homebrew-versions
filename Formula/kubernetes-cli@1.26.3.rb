874ebe81c12e330fba9a98be85420be542e1b450
Richard Johnson <uthacalthing@gmail.com>
2023-04-16 07:51:13 -0600

    Repair class kubernetes-cli@1.26.3

--- Formula/kubernetes-cli@1.26.3.rb
+++ Formula/kubernetes-cli@1.26.3.rb
@@ -1,4 +1,4 @@
-class KubernetesCliAT1261 < Formula
+class KubernetesCliAT1263 < Formula
   desc "Kubernetes command-line interface"
   homepage "https://kubernetes.io/"
   url "https://github.com/kubernetes/kubernetes.git",
